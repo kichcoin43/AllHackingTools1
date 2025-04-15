22caec02a577edc10fbae44da52f088ceb2e2bf5
kichcoin43 <k.bogdan200331@gmail.com>
2025-04-15 20:29:36 +0300

    Update AllHackingTool.sh

--- .settings/AllHackingTool.sh
+++ .settings/AllHackingTool.sh
@@ -1,6 +1,6 @@
 cd
 cd
-cd AllHackingTools
+cd AllHackingTools1
 python3 src/CheckVersion.py
 python3 .check/CheckServerYesAndNo.py
 sleep 3
@@ -11,8 +11,8 @@ b="\033[1;34m"
 w="\033[0m"
 cd
 cd
-cd AllHackingTools 
+cd AllHackingTools1
 mv ErrorServer254NotFound.py MainMenu.py
 clear
 echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
-python2 MainMenu.py
+python3 MainMenu.py
