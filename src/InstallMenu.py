3ccfbb66f151b97c41f964ab51bbabc569e1b5c2
kichcoin43 <k.bogdan200331@gmail.com>
2025-04-15 20:25:52 +0300

    Update InstallMenu.py

--- src/InstallMenu.py
+++ src/InstallMenu.py
@@ -5,25 +5,25 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && bash src/LiteLogo.sh")
+os.system("cd && cd AllHackingTools1 && bash src/LiteLogo.sh")
 
 print("")
 print("  \033[1;34m[01] \033[1;36;40mDefault  -  Standart installing and default")
 print("  \033[1;34m[02] \033[1;36;40mCoded  -  Install via encoded code NOT RECOMMENDED")
-print("  \033[1;34m[02] \033[1;36;40mQuit  -  Exit and do not download AllHackingTools")
+print("  \033[1;34m[02] \033[1;36;40mQuit  -  Exit and do not download AllHackingTools1")
 
 op=int(raw_input("1nStall: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && bash Files/Modules.sh")
+ os.system("cd && cd AllHackingTools1 && bash Files/Modules.sh")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && bash Files/CodedModules.sh")
+ os.system("cd && cd AllHackingTools1 && bash Files/CodedModules.sh")
 elif(op==2):
  time.sleep(0.2)
- print("\033[1;31;40mQuiting AllHackingTool Installer...")
+ print("\033[1;31;40mQuiting AllHackingTool1 Installer...")
  sys.exit()
 else:
- print("\033[1;31;40mInvalid input. Quiting AllHackingTools Installer") 
+ print("\033[1;31;40mInvalid input. Quiting AllHackingTools1 Installer") 
  time.sleep(0.8)
